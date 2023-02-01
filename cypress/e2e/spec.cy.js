0c6774cf931fe041a62a7f4d841d7891d18cd145
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-02-01 10:58:27 +0000

    random email package add to generate unique email addresses

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,19 +1,22 @@
-describe('registration', () => {
+describe('registration', function() {
 
-  beforeEach(() => {
+    const email = require('random-email');
+    const password = '1234567';
+
+  it('user successfully registers ', function()
+  {
     cy.visit('http://localhost:8000/register')
-    cy.get('[id=registration_form_email]').type('test200@email.co')
-    cy.get('[id=registration_form_plainPassword]').type('1234567')
-  })
 
-  it('user successfully registers ', () => {
-
+    cy.get('[id=registration_form_email]').type(email({domain: 'example.com'}))
+    cy.get('[id=registration_form_plainPassword]').type(password)
     cy.get('[type="checkbox"]').check()
+
+
 
     cy.get('#registerButton').click()
 
     //user should be redirected to /dashboard
     cy.url().should('include', '/dashboard')
     cy.url().should('eq', 'http://localhost:8000/dashboard')
-  })
-})
+  });
+});
