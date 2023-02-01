1cb7cda0247487e37464702797ca99f2b77a7e60
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-02-01 11:15:13 +0000

    test added to check user cant register with empty fields

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe('registration', function() {
+describe('registration page', function() {
 
     const email = require('random-email');
     const password = '1234567';
@@ -19,4 +19,21 @@ describe('registration', function() {
     cy.url().should('include', '/dashboard')
     cy.url().should('eq', 'http://localhost:8000/dashboard')
   });
+
+  it('user cant register with empty fields', function()
+  {
+    cy.visit('http://localhost:8000/register')
+
+    cy.get('[id=registration_form_email]').should('not.have.value')
+    cy.get('[id=registration_form_plainPassword]').should('not.have.value')
+    cy.get('[type="checkbox"]').check()
+
+    cy.get('#registerButton').click()
+
+    //user should see prompt to fill in field
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Please fill in this field`)
+    })
+  });
+
 });
