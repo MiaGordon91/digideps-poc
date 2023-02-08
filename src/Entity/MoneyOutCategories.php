956e1295257c93233977487e82bcbe5de1a12824
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-02-08 13:57:55 +0000

    money out entity and controller created

--- src/Entity/MoneyOutCategories.php
+++ src/Entity/MoneyOutCategories.php
@@ -10,6 +10,24 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: MoneyOutCategoriesRepository::class)]
 class MoneyOutCategories
 {
+    public static function getPaymentCategories(): array
+    {
+        return [
+                'Care Fees' => 'Care or medical bill',
+                'Clothes' => 'Personal expenses',
+                'Broadband' => 'Household bills and expenses',
+                'Council Tax' => 'Household bills and expenses',
+                'Electricity' => 'Household bills and expenses',
+                'Food' => 'Household bills and expenses',
+                'Rent' => 'Accommodation costs',
+                'Medical Expenses' => 'Care or medical bill',
+                'Mortgage' => 'Accommodation costs',
+                'Personal Allowance' => 'Personal expenses',
+                'Water' => 'Household bills and expenses',
+                'Wifi' => 'Household bills and expenses',
+            ];
+    }
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -24,14 +42,43 @@ class MoneyOutCategories
     #[ORM\OneToMany(mappedBy: 'category_type', targetEntity: MoneyOut::class)]
     private Collection $categoryTypes;
 
-    public function __construct()
+    public function __construct(string $payment_category, string $payment_type)
     {
-        $this->categoryTypes = new ArrayCollection();
+//        $this->categoryTypes = new ArrayCollection();
+        $this->payment_category = $payment_category;
+        $this->payment_type = $payment_type;
+    }
+
+    public function getMoneyOutCategory()
+    {
+        foreach (self::getPaymentCategories() as $paymentCategory => $paymentType) {
+            if ($paymentCategory == $this->payment_category) {
+                return $paymentCategory;
+            }
+        }
+
+        return $this->payment_category;
+    }
+
+    public function getMoneyOutType()
+    {
+        foreach (self::getPaymentCategories() as $paymentCategory => $paymentType) {
+            if ($paymentType == $this->payment_type) {
+                return $paymentType;
+            }
+        }
+
+        return $this->payment_type;
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
     }
 
     public function getPaymentCategory(): ?string
@@ -58,33 +105,34 @@ class MoneyOutCategories
         return $this;
     }
 
-    /**
-     * @return Collection<int, MoneyOut>
-     */
-    public function getPaymentCategories(): Collection
-    {
-        return $this->categoryTypes;
-    }
-
-    public function addPaymentCategory(MoneyOut $categoryTypes): self
-    {
-        if (!$this->categoryTypes->contains($categoryTypes)) {
-            $this->categoryTypes->add($categoryTypes);
-            $categoryTypes->setPaymentCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removePaymentCategory(MoneyOut $categoryTypes): self
-    {
-        if ($this->categoryTypes->removeElement($categoryTypes)) {
-            // set the owning side to null (unless already changed)
-            if ($categoryTypes->getPaymentCategory() === $this) {
-                $categoryTypes->setPaymentCategory(null);
-            }
-        }
-
-        return $this;
-    }
+//
+//    /**
+//     * @return Collection<int, MoneyOut>
+//     */
+//    public function getPaymentCategories(): Collection
+//    {
+//        return $this->categoryTypes;
+//    }
+//
+//    public function addPaymentCategory(MoneyOut $categoryTypes): self
+//    {
+//        if (!$this->categoryTypes->contains($categoryTypes)) {
+//            $this->categoryTypes->add($categoryTypes);
+//            $categoryTypes->setPaymentCategory($this);
+//        }
+//
+//        return $this;
+//    }
+//
+//    public function removePaymentCategory(MoneyOut $categoryTypes): self
+//    {
+//        if ($this->categoryTypes->removeElement($categoryTypes)) {
+//            // set the owning side to null (unless already changed)
+//            if ($categoryTypes->getPaymentCategory() === $this) {
+//                $categoryTypes->setPaymentCategory(null);
+//            }
+//        }
+//
+//        return $this;
+//    }
 }
