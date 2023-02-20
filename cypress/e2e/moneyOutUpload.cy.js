9b3bf02cc240cf814caf0e4fdd5035b977429449
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-02-20 11:44:19 +0000

    upload functionality tested

--- cypress/e2e/moneyOutUpload.cy.js
+++ cypress/e2e/moneyOutUpload.cy.js
@@ -1,4 +1,4 @@
-describe('money out download', () => {
+describe('money out upload', () => {
 
     beforeEach(() => {
         cy.visit('http://localhost:8000/register')
@@ -7,7 +7,7 @@ describe('money out download', () => {
     const generateEmail = require('random-email');
     const password = '1234567';
 
-    it('user successfully downloads money out file', () => {
+    it('user successfully uploads money out file', () => {
 
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false;
@@ -15,9 +15,7 @@ describe('money out download', () => {
 
         cy.get('[id=registration_form_email]').type(generateEmail({domain: 'example.com'}))
         cy.get('[id=registration_form_plainPassword]').type(password)
-
         cy.get('#registerButton').click()
-        //user should be redirected to /money_out
         cy.url().should('eq', 'http://localhost:8000/money_out')
 
         //Event listener added for 'click' which fires a page reload and triggers page load event
@@ -30,6 +28,10 @@ describe('money out download', () => {
             cy.get('#downloadMoneyOut').click()
             cy.verifyDownload('money_out_template.xlsx');
 
+            const filepath = './cypress/downloads/money_out_template.xlsx';
+            cy.get('input[type=file]').selectFile(filepath)
+            cy.get('#upload').click()
+            cy.get('#flashMessage').should('be.visible')
+        });
     });
-   });
 });
